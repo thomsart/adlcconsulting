fa117ccba35d670e15d3bd8ca6277ddb4edc5799
thomsart <psychoid@hotmail.fr>
2022-04-11 15:26:24 +0200

    flask en local OK

--- launch.py
+++ launch.py
@@ -3,37 +3,26 @@
 
 This module is use to execute the server of the site www.thomsart.tech """
 
-import bottle
-from bottle import Bottle, route, static_file, template, run
-from paste import httpserver
+from flask import Flask, render_template
 
-app = Bottle()
 
-@app.route('<filename:path>')
-def return_css(filename):
-    """ The path to return css files """
-
-    if str(filename) == "/base.css":
-        return static_file(filename, root='static/css/')
-    else:
-        return static_file(filename, root='static/assets/img/')
-
+app = Flask('__name__')
 
 @app.route('/')
 def home():
     """ The path to return the home template """
 
-    return template('home.', name=home)
+    return render_template('home.html', name=home)
 
 
 @app.route('/mentions_legales')
 def mentions_legales():
     """ The path to return the mentions legales template """
 
-    return template('mentions_legales', name=mentions_legales)
+    return render_template('mentions_legales.html', name=mentions_legales)
 
 
 if __name__ == '__main__':
-    run(app, host='localhost', port=8000, debug=True, reloader=True)
+    app.run(debug=True)
 else:
-    httpserver.serve(app, host='92.222.167.113', port=8001)#app.run(server='gunicorn')
+    app.run(debug=False)
