e7748ce1f9c5548203ae72f87e3e9260e620cc93
thomsart <psychoid@hotmail.fr>
2022-04-12 13:29:21 +0200

    envoie sur VPS

--- web_site/app.py
+++ web_site/app.py
@@ -5,24 +5,23 @@ This module is use to execute the server of the site www.thomsart.tech """
 
 from flask import Flask, render_template
 
-
-app = Flask('__name__')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     """ The path to return the home template """
 
-    return render_template('home.html', name=home)
+    return render_template('home.html')
 
 
 @app.route('/mentions_legales')
 def mentions_legales():
     """ The path to return the mentions legales template """
 
-    return render_template('mentions_legales.html', name=mentions_legales)
+    return render_template('mentions_legales.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 else:
-    app.run(debug=False)
+    print('Sur le server!')
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+"""
+This module is use to launch the deployement server.
+"""
+
+import web_site.app
